9d4230248e8d9e63be40dad129e87ee6675ca4aa
GindullinRuslan <rusyazlatik@mail.ru>
2022-02-20 19:45:22 +0500

    lesson 2 var3

--- script.js
+++ script.js
@@ -87,9 +87,35 @@ const personalMovieDB = {
 // }
 
 
+// let i = 0;
+
+// while (i < 2) {
+//   const a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = prompt('На сколько оцените его?', '');
+
+//   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     i++;
+//     console.log('OK');
+//   } else {
+//     console.log('Error');
+//   }
+// }
+
+
+// if (personalMovieDB.count < 10) {
+//   console.log('Просмотрено довольно мало фильмов');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//   console.log('Вы классический зритель');
+// } else if (personalMovieDB.count >= 30) {
+//   console.log('Вы киноман');
+// } else {
+//   console.log('Произошла ошибка');
+// }
+
 let i = 0;
 
-while (i < 2) {
+do {
   const a = prompt('Один из последних просмотренных фильмов?', ''),
     b = prompt('На сколько оцените его?', '');
 
@@ -101,6 +127,8 @@ while (i < 2) {
     console.log('Error');
   }
 }
+
+while (i < 2);
 
 
 if (personalMovieDB.count < 10) {
