e3e98111b54bf47ee6a47fcc49d10c7187fbcf7e
GindullinRuslan <rusyazlatik@mail.ru>
2022-02-20 19:36:36 +0500

    lesson 2 var2

--- script.js
+++ script.js
@@ -62,19 +62,46 @@ const personalMovieDB = {
   privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
+// for (let i = 0; i < 2; i++) {
+//   const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
 
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-          personalMovieDB.movies[a] = b;
-          console.log('OK');
-        } else  {
-          console.log('Error');
-          i--;
-        }
-        
+//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//           personalMovieDB.movies[a] = b;
+//           console.log('OK');
+//         } else  {
+//           console.log('Error');
+//           i--;
+//         }
+
+// }
+
+// if (personalMovieDB.count < 10) {
+//   console.log('Просмотрено довольно мало фильмов');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//   console.log('Вы классический зритель');
+// } else if (personalMovieDB.count >= 30) {
+//   console.log('Вы киноман');
+// } else {
+//   console.log('Произошла ошибка');
+// }
+
+
+let i = 0;
+
+while (i < 2) {
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
+
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    i++;
+    console.log('OK');
+  } else {
+    console.log('Error');
+  }
 }
+
 
 if (personalMovieDB.count < 10) {
   console.log('Просмотрено довольно мало фильмов');
